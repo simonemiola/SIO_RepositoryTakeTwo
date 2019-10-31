fb6dbe463eaf911d79732cb4ffa0c09c0a20e9e0
baduCleo <baduCleo@users.noreply.github.com>
2019-10-31 16:36:13 +0100

    Repositories Added

---
+++ SIO/src/main/java/com/jtr/sio/model/repositories/RicoveroRepository.java
@@ -0,0 +1,7 @@
+package com.jtr.sio.model.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RicoveroRepository extends JpaRepository<Ricovero, Long> {
+
+}
